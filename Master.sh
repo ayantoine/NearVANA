4954202352353bea0aa587432971192da2862ffc
ayantoine <aymeric.antoine@hotmail.fr>
2019-10-22 10:29:15 +0200

    Add: Define and check base arg and conf

---
+++ Master.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+ARG=$1
+#WARNING!! Source file is executed (Security, etc.)
+source $ARG
+
+echo "------ Check Input existence ------"
+if [ ! -d $SDIR ] ; then
+	echo "Directory SDIR ${SDIR} does not exists"
+	exit 1
+fi
+LIST_FILE=($R1 $R2 $ADAP $DODE $META $SUBS $NUCACC $NUCDEF $PROACC $PRODEF $DBLINEAGE $CONF)
+for i in "${LIST_FILE[@]}"; do
+	if [ ! -f $i ]; then
+		echo "File $i does not exists"
+		exit 1
+done
+
+List_NONFILE=(ID R1 R2 ADAP DODE META SUBS NUCACC NUCDEF PROACC PRODEF DBLINEAGE CONF)
+for i in "${LIST_PARAM[@]}"; do
+	echo "$i: ${!i}"
+done
+
+source $CONF
+LIST_PARAM=(SCALL SPARAM STASKARRAY SMAXTASK SRENAME SMAXSIMJOB STASKID SPSEUDOTASKID VIRNTDB ALLNTDB VIRPTDB ALLPTDB)
+for i in "${LIST_PARAM[@]}"; do
+	echo "$i: ${!i}"
+done
+echo "------ /Check Input existence ------"
+
