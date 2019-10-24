99206e5b3914cb4376ce636d90364afa14e28eb3
ayantoine <aymeric.antoine@hotmail.fr>
2019-10-24 14:35:14 +0200

    wip

--- Substraction.sh
+++ Substraction.sh
@@ -14,6 +14,10 @@ echo "------ Mapping ------"
 bowtie2 --end-to-end --very-sensitive -x ${SUBS} -1 ${PID}_R1.Uncorrected.fastq -2 ${PID}_R2.Uncorrected.fastq -U ${PID}_R0.Uncorrected.fastq -S ${PID}"_"${SUBS}"_"${PID}_R1.Corrected.fastq"_"${PID}_R2.Corrected.fastq"_bwt_ete.sam"
 echo "------ /Mapping ------"
 
+echo "------ Mapping ------"
+python MappingExtraction.py -p ${PID} -i ${PID}"_"${SUBS}"_"${PID}_R1.Corrected.fastq"_"${PID}_R2.Corrected.fastq"_bwt_ete.sam"
+echo "------ /Mapping ------"
+
 datetime2=$(date +%s)
 delta=$((datetime2 - datetime1))
-echo "Time RetrievePair: "$delta > Time06.txt
+echo "Time Mapping against PhiX: "$delta > Time06.txt
