1bb51da9bb0cd86e22fa868815b4c60e5cbf0a51
ayantoine <aymeric.antoine@hotmail.fr>
2019-11-12 12:37:08 +0100

    wip

--- CreateTable.sh
+++ CreateTable.sh
@@ -9,4 +9,7 @@ source $CONF
 task=$2
 nb_jobs=$3
 
+echo "python ${SDIR}/CreateTable.py -t ${task} -j ${nb_jobs} -p ${PID} -m ${META}"
 python ${SDIR}/CreateTable.py -t ${task} -j ${nb_jobs} -p ${PID} -m ${META}
+
+touch ${PID}.creation${task}.ok
