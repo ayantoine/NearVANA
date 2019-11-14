5d22dc2c8c161e2b60e5ac05e2fb54a1149f7d1f
ayantoine <aymeric.antoine@hotmail.fr>
2019-11-14 11:53:53 +0100

    wip

--- GetTaxonomy.sh
+++ GetTaxonomy.sh
@@ -27,16 +27,16 @@ for Task in ${TASK_ARRAY[@]}; do
     for LINE in $(cut -f2 ${FilePath}); do
 	ACC=$(echo $LINE | cut -d'|' -f4)
 	echo ${ACC}
-	echo ^${ACC} > ${ACC}.target.txt
-	while [ ! -e ${ACC}.target.txt ]; do sleep 1 ; done
+	echo ^${ACC} > ${STASKID}.${ACC}.target.txt
+	while [ ! -e ${STASKID}.${ACC}.target.txt ]; do sleep 1 ; done
 	
-	TAXID=$(grep -m 1 -f ${ACC}.target.txt ${DBTARGET} | cut -f2)
-	echo "^"${TAXID}"\t" > ${ACC}.taxid.txt 
-	while [ ! -e ${ACC}.taxid.txt ]; do sleep 1 ; done
-	grep -m 1 -P -f ${ACC}.taxid.txt ${DBLINEAGE} > ${ACC}.lineage.txt
-	while [ ! -e ${ACC}.lineage.txt ]; do sleep 1 ; done
-	ACCorganism=$(cut -f2 ${ACC}.lineage.txt)
-	ACClineage=$(cut -f3 ${ACC}.lineage.txt)
+	TAXID=$(grep -m 1 -f ${STASKID}.${ACC}.target.txt ${DBTARGET} | cut -f2)
+	echo "^"${TAXID}"\t" > ${STASKID}.${ACC}.taxid.txt 
+	while [ ! -e ${STASKID}.${ACC}.taxid.txt ]; do sleep 1 ; done
+	grep -m 1 -P -f ${STASKID}.${ACC}.taxid.txt ${DBLINEAGE} > ${STASKID}.${ACC}.lineage.txt
+	while [ ! -e ${STASKID}.${ACC}.lineage.txt ]; do sleep 1 ; done
+	ACCorganism=$(cut -f2 ${STASKID}.${ACC}.lineage.txt)
+	ACClineage=$(cut -f3 ${STASKID}.${ACC}.lineage.txt)
 	ACCsupKingdom=$(echo ${ACClineage} | cut -d ';' -f1)
 	
 	# Viruses = first element of the lineage. But for others, its "cellular organisms"
@@ -48,23 +48,23 @@ for Task in ${TASK_ARRAY[@]}; do
 	ACCdefinition="."
 	if [ "${ACCsupKingdom}" == "Viruses"  ]; then
 	    #First try, on persistant file
-	    ACCdefinition=$(grep -m 1 -f ${ACC}.target.txt ${DBDEF} | cut -f2)
+	    ACCdefinition=$(grep -m 1 -f ${STASKID}.${ACC}.target.txt ${DBDEF} | cut -f2)
 	    if [ ! -n ${ACCdefinition} ] ; then
 		#Second try, on temporary file
-		ACCdefinition=$(grep -m 1 -f ${ACC}.target.txt ${TempDefFile} | cut -f2)
+		ACCdefinition=$(grep -m 1 -f ${STASKID}.${ACC}.target.txt ${TempDefFile} | cut -f2)
 		if [ ! -n ${ACCdefinition} ] ; then        
 		    echo "Unkown AccID ${ACC} in ${DBDEF}"
 		    echo "Ask ebi"
 		    echo "curl -s -N https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=${TAG}&id=${ACC}&rettype=gb&retmode=text"
-		    curl -s -N "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=${TAG}&id=${ACC}&rettype=gb&retmode=text" > ${ACC}.${TAG}.def
-		    ACCdefinition=$(grep -m 1 -f DEFINITION.txt ${ACC}.${TAG}.def | cut -c 13-)
+		    curl -s -N "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=${TAG}&id=${ACC}&rettype=gb&retmode=text" > ${STASKID}.${ACC}.${TAG}.def
+		    ACCdefinition=$(grep -m 1 -f DEFINITION.txt ${STASKID}.${ACC}.${TAG}.def | cut -c 13-)
 		    if [ ! -n ${ACCdefinition} ] ; then
 			echo "Unable to download definition from EBI for ${ACC}"
 			ACCdefinition="#N/D"
 		    else
 			printf "${ACC}\t${ACCdefinition}\n" >> ${TempDefFile}
 		    fi
-		    rm ${ACC}.${TAG}.def
+		    rm ${STASKID}.${ACC}.${TAG}.def
 		fi
 	    fi
 	fi
@@ -72,9 +72,9 @@ for Task in ${TASK_ARRAY[@]}; do
 	result="${ACC}\t${ACCorganism}\t${ACCsupKingdom}\t${ACClineage}\t${ACCdefinition}\n"
 	printf "${result}" >> ${FilePath}.taxo
 	
-	rm ${ACC}.target.txt
-	rm ${ACC}.taxid.txt
-	rm ${ACC}.lineage.txt
+	rm ${STASKID}.${ACC}.target.txt
+	rm ${STASKID}.${ACC}.taxid.txt
+	rm ${STASKID}.${ACC}.lineage.txt
 		
     done
 done
