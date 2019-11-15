137c0bad018df06e66b507dfb1feb5e9763a459b
ayantoine <aymeric.antoine@hotmail.fr>
2019-11-15 11:20:23 +0100

    wip

--- CreateTableFusion.py
+++ CreateTableFusion.py
@@ -113,222 +113,7 @@ SHORTFLASH=sPID+"_All.FLASH.contigs2sample.tsv"
 
 ########################################################################
 #Function 	
-# def LoadMetadata(sFile):
-	# print("Loading file "+str(sFile))
-	# dDict={}
-	# for sNewLine in open(sFile):
-		# sLine=sNewLine.replace("\n","")
-		# if len(sLine)==0:
-			# continue
-		# tLine=sLine.split("\t")
-		
-		# sHost=DEFAULT
-		# if tLine[META_HOSTCOL]!="":
-			# sHost=tLine[META_HOSTCOL]
-		# sLocation=DEFAULT
-		# if tLine[META_LOCATIONCOL]!="":
-			# sLocation=tLine[META_LOCATIONCOL]
-		# sDate=DEFAULT
-		# if tLine[META_DATECOL]!="":
-			# sDate=tLine[META_DATECOL]
-		# sIndividuals=DEFAULT
-		# if tLine[META_INDIVIDUALSCOL]!="":
-			# sIndividuals=tLine[META_INDIVIDUALSCOL]
-		# sWeight=DEFAULT
-		# if tLine[META_WEIGHTCOL]!="":
-			# sWeight=tLine[META_WEIGHTCOL]
-		
-		# dDict[tLine[META_SAMPLECOL]]={
-			# "Host":sHost,
-			# "Location":sLocation,
-			# "Date":sDate,
-			# "Individuals":sIndividuals,
-			# "Weight":sWeight
-			# }
-			
-	# return dDict
-
-# def LoadContigs(sFile,dRef,dDict={}):
-	# print("Loading file "+str(sFile))
-	# for sNewLine in open(sFile):
-		# sLine=sNewLine.strip()
-		# tLine=sLine.split()
-		# sName=tLine[0]
-		# try:
-			# oCrash=dRef[sName]
-			# dDict[sName]=tLine[1].split(SAMPLE_SEPARATOR)
-		# except KeyError:
-			# continue
-	# return dDict
-
-# def LoadTaxo(sFile):
-	# print("Loading file "+str(sFile))
-	# dDict={}
-	# for sNewLine in open(sFile):
-		# sLine=sNewLine.strip()
-		# tLine=sLine.split("\t")
-		
-		# # print(tLine)
-		# sOrganism=DEFAULT
-		# if tLine[TAXO_ORGANISMCOL]!="":
-			# sOrganism=tLine[TAXO_ORGANISMCOL]
-		# sSuperkingdom=DEFAULT
-		# if tLine[TAXO_SUPKINGDOMCOL]!="":
-			# sSuperkingdom=tLine[TAXO_SUPKINGDOMCOL]
-		# sLineage=DEFAULT
-		# if tLine[TAXO_LINEAGECOL]!="":
-			# sLineage=tLine[TAXO_LINEAGECOL]
-		# sDefinition=DEFAULT
-		# if tLine[TAXO_DEFCOL]!="":
-			# sDefinition=tLine[TAXO_DEFCOL]
-		
-		
-		# dDict[tLine[TAXO_ACCCOL]]={
-			# "Organism":sOrganism,
-			# "Superkingdom":sSuperkingdom,
-			# "Lineage":sLineage,
-			# "Definition":sDefinition
-			# }
-			
-	# return dDict
-
-# def LoadBlast(sFile):
-	# print("Loading file "+str(sFile))
-	# dDict={}
-	# for sNewLine in open(sFile):
-		# sLine=sNewLine.strip()
-		# tLine=sLine.split("\t")
-		# sQueryId=tLine[BLAST_QUERYIDCOl]
-		# try:
-			# oCrash=dDict[sQueryId]
-		# except KeyError:
-			# dDict[sQueryId]={}
-		
-		# sSubjectId=DEFAULT
-		# if tLine[BLAST_SUBJECTIDCOl]!="":
-			# sSubjectId=tLine[BLAST_SUBJECTIDCOl]
-			# tSubjectId=sSubjectId.split("|")
-			# sSubjectId=tSubjectId[3]
-		# sIdentity=DEFAULT
-		# if tLine[BLAST_IDENTITYCOl]!="":
-			# sIdentity=tLine[BLAST_IDENTITYCOl]
-		# sLength=DEFAULT
-		# if tLine[BLAST_LENGTHCOl]!="":
-			# sLength=tLine[BLAST_LENGTHCOl]
-		# sMismatch=DEFAULT
-		# if tLine[BLAST_MISMATCHCOl]!="":
-			# sMismatch=tLine[BLAST_MISMATCHCOl]
-		# sGapOpen=DEFAULT
-		# if tLine[BLAST_GAPOPENCOl]!="":
-			# sGapOpen=tLine[BLAST_GAPOPENCOl]
-		# sQueryStart=DEFAULT
-		# if tLine[BLAST_QUERYSTARTCOl]!="":
-			# sQueryStart=tLine[BLAST_QUERYSTARTCOl]
-		# sQueryEnd=DEFAULT
-		# if tLine[BLAST_QUERYENDCOl]!="":
-			# sQueryEnd=tLine[BLAST_QUERYENDCOl]
-		# sSubjectStart=DEFAULT
-		# if tLine[BLAST_SUBJECTSTARTCOl]!="":
-			# sSubjectStart=tLine[BLAST_SUBJECTSTARTCOl]
-		# sSubjectEnd=DEFAULT
-		# if tLine[BLAST_SUBJECTENDCOl]!="":
-			# sSubjectEnd=tLine[BLAST_SUBJECTENDCOl]
-		# sEvalue=DEFAULT
-		# if tLine[BLAST_EVALUECOl]!="":
-			# sEvalue=tLine[BLAST_EVALUECOl]
-		# sBitScore=DEFAULT
-		# if tLine[BLAST_BITSCORECOl]!="":
-			# sBitScore=tLine[BLAST_BITSCORECOl]
-					
-		# dDict[sQueryId][len(dDict[sQueryId])+1]={
-			# "SubjectId":sSubjectId,
-			# "Identity":sIdentity,
-			# "Length":sLength,
-			# "Mismatch":sMismatch,
-			# "GapOpen":sGapOpen,
-			# "QueryStart":sQueryStart,
-			# "QueryEnd":sQueryEnd,
-			# "SubjectStart":sSubjectStart,
-			# "SubjectEnd":sSubjectEnd,
-			# "Evalue":sEvalue,
-			# "BitScore":sBitScore
-			# }
-	# return dDict
-
-# def LoadQuery(sFile):
-	# print("Loading file "+str(sFile))
-	# dDict={}
-	# sSeqContent=""
-	# sSeqName=""
-	# for sNewLine in open(sFile):
-		# if ">"==sNewLine[0]:
-			# if sSeqName!="":
-				# dDict[sSeqName]=sSeqContent
-			# sSeqName=sNewLine[1:-1]
-			# sSeqContent=""
-		# else:
-			# sSeqContent+=sNewLine[:-1] #:-1, for \n char
-	# if ">"==sNewLine[0]:
-		# if sSeqName!="":
-			# dDict[sSeqName]=sSeqContent
-	# return dDict
-
-# def WriteData(FILE,dBlast,dTaxo,dContigs,dMetadata,dContent,dLength):
-	# for sQuery in dBlast:
-		# for iRank in dBlast[sQuery]:
-			
-			# if iRank==1:
-				# sRank=BEST_HIT
-			# else:
-				# sRank=HIT
-			
-			# iQuerySize=len(dContent[sQuery])
-			# iCoverSize=int(dBlast[sQuery][iRank][QueryEnd])-int(dBlast[sQuery][iRank][QueryStart])
-			# fCover=round(float(iCoverSize)/iQuerySize*100,2)
-			
-			# try:
-				# tSample=dContigs[sQuery]
-			# except KeyError:
-				# tSample=[sQuery.split("_")[-1]]
-			
-			# sReadQuantity=sQuery.split("(")[-1].split(")")[0]
-			# sSubjectId=dBlast[sQuery][iRank]["SubjectId"]
-			
-			# sTaxo=dTaxo[sSubjectId]["Lineage"]
-			# tTaxo=sTaxo.replace("; ",";").split(";")
-			# iMinSize=0
-			# for oTaxo in tTaxo:
-				# try:
-					# iMinSize=dLength[oTaxo]
-				# except KeyError:
-					# continue
-			# if iMinSize==0:
-				# fFragment="N/A"
-			# else:
-				# fFragment=round(float(iQuerySize)/iMinSize*100,2)
-				
-			# for sSample in tSample:
-				# tLine=[sRank,sQuery,sSample,sReadQuantity,str(iQuerySize),
-				# dMetadata[sSample]["Location"],dMetadata[sSample]["Date"],
-				# dMetadata[sSample]["Host"],dMetadata[sSample]["Individuals"],
-				# dMetadata[sSample]["Weight"],sSubjectId,
-				# dTaxo[sSubjectId]["Organism"],dTaxo[sSubjectId]["Superkingdom"],
-				# dTaxo[sSubjectId]["Lineage"],dTaxo[sSubjectId]["Definition"],dBlast[sQuery][iRank]["Confidence"],
-				# str(fFragment),dBlast[sQuery][iRank]["Identity"],
-				# str(fCover),dBlast[sQuery][iRank]["Length"],dBlast[sQuery][iRank]["Mismatch"],
-				# dBlast[sQuery][iRank]["GapOpen"],dBlast[sQuery][iRank]["QueryStart"],
-				# dBlast[sQuery][iRank]["QueryEnd"],dBlast[sQuery][iRank]["SubjectStart"],
-				# dBlast[sQuery][iRank]["SubjectEnd"],dBlast[sQuery][iRank]["Evalue"],
-				# dBlast[sQuery][iRank]["BitScore"],dContent[sQuery]
-				# ]
-				# FILE.write("\t".join(tLine)+"\n")
-						
-# HEADER_LIST=["Hit rank","Query Seq-Id","Sample","Read quantity","Sequence length","Location","Date","Host",
-# "Individual","Weight(mg)","Subject Seq-Id","Organism","SuperKingdom","Taxonomy","Hit definition","Identification","% Fragment","Identity",
-# "Query cover","Alignment length","Mismatches","Gap opening","Start alignment query","End alignment query",
-# "Start alignment subject","End alignment subject","E-value","Bit score","Query sequences"]
-
-def FusionTaxoDict(dDict1,dDict2):
+def FusionDict(dDict1,dDict2):
 	dDict={}
 	for sKey in dDict1:
 		dDict[sKey]=dDict1[sKey]
@@ -406,18 +191,7 @@ def FusionBlastDict(dBlastN,dBlastX):
 					dDict[sKey][iNewRank]=dBlastN[sSubjectId][iRank]
 					dDict[sKey][iNewRank]["Confidence"]=dSubjectId2Confidence[sSubjectId]
 					
-	return dDict
-
-# def LoadLength(sFile):
-	# print("Loading file "+str(sFile))
-	# dDict={}
-	# for sNewLine in open(sFile):
-		# sLine=sNewLine.strip()
-		# tLine=sLine.split()
-		# sFamily=tLine[0]
-		# iMinSize=int(tLine[1])
-		# dDict[sFamily]=iMinSize
-	# return dDict					
+	return dDict				
 
 ########################################################################
 #MAIN
@@ -427,14 +201,16 @@ if __name__ == "__main__":
 	FILE=open(BLASTALL_OUTPUT,"w")
 	FILE.write(HEADER)
 	for iIndex in range(1,iJobs+1):
-		dQuery2Content=LoadQuery(BLAST_FOLDER+"/"+BLAST_INPUT.replace(REPLACEME,str(iIndex)))
+		dQueryN2Content=LoadQuery(BLASTN_FOLDER+"/"+BLAST_INPUT.replace(REPLACEME,str(iIndex)))
+		dQueryX2Content=LoadQuery(BLASTX_FOLDER+"/"+BLAST_INPUT.replace(REPLACEME,str(iIndex)))
 		dContigs2Sample=LoadContigs(SHORTSPADES,dQuery2Content)
 		dContigs2Sample=LoadContigs(SHORTFLASH,dQuery2Content,dContigs2Sample)
 		dTaxoN=LoadTaxo(BLASTN_FOLDER+"/"+TAXON_FILE.replace(REPLACEME,str(iIndex)))
 		dTaxoX=LoadTaxo(BLASTX_FOLDER+"/"+TAXOX_FILE.replace(REPLACEME,str(iIndex)))
 		dBlastN=LoadBlast(BLASTN_FOLDER+"/"+BLASTN_FILE.replace(REPLACEME,str(iIndex)))
 		dBlastX=LoadBlast(BLASTX_FOLDER+"/"+BLASTX_FILE.replace(REPLACEME,str(iIndex)))
-		dTaxo=FusionTaxoDict(dTaxoN,dTaxoX)
+		dQuery2Content=FusionDict(dQueryN2Content,dQueryX2Content)
+		dTaxo=FusionDict(dTaxoN,dTaxoX)
 		dBlast=FusionBlastDict(dBlastN,dBlastX)
 		WriteData(FILE,dBlast,dTaxo,dContigs2Sample,dMetadata,dQuery2Content,dLength)
 	FILE.close()
