d4297b12e99f2393df821cd9043094b3ae9ff483
ayantoine <aymeric.antoine@hotmail.fr>
2019-11-15 10:54:45 +0100

    wip

--- CreateTableFusion.py
+++ CreateTableFusion.py
@@ -4,11 +4,15 @@
 
 import time
 from optparse import OptionParser
+from CreateTable import *
 
 sCurrentVersionScript="v1"
 iTime1=time.time()
 ########################################################################
 '''
+V2-2019/11/15
+Import function from basic script
+
 V1-2019/11/06
 Create tsv bilan from BlastX and BlastN data
 
@@ -18,45 +22,47 @@ PID: Plaque Id
 '''
 ########################################################################
 #CONSTANT
-HEADER_LIST=["Hit rank","Query Seq-Id","Sample","Read quantity","Sequence length","Location","Date","Host",
-"Individual","Weight(mg)","Subject Seq-Id","Organism","SuperKingdom","Taxonomy","Hit definition","Identification","% Fragment","Identity",
-"Query cover","Alignment length","Mismatches","Gap opening","Start alignment query","End alignment query",
-"Start alignment subject","End alignment subject","E-value","Bit score","Query sequences"]
+# HEADER_LIST=["Hit rank","Query Seq-Id","Sample","Read quantity","Sequence length","Location","Date","Host",
+# "Individual","Weight(mg)","Subject Seq-Id","Organism","SuperKingdom","Taxonomy","Hit definition","% Fragment","Identity",
+# "Query cover","Alignment length","Mismatches","Gap opening","Start alignment query","End alignment query",
+# "Start alignment subject","End alignment subject","E-value","Bit score","Query sequences"]
 
-HEADER="\t".join(HEADER_LIST)+"\n"
+# HEADER="\t".join(HEADER_LIST)+"\n"
 
-BEST_HIT="Best hit"
-HIT="."
+# BEST_HIT="Best hit"
+# HIT="."
 
-REPLACEME="REPLACE-ME"
-SAMPLE_SEPARATOR="-"
-CONTIG="Contig"
+# REPLACEME="REPLACE-ME"
+# SAMPLE_SEPARATOR="-"
+# CONTIG="Contig"
 
-META_SAMPLECOL=0
-META_HOSTCOL=1
-META_LOCATIONCOL=3
-META_DATECOL=6
-META_INDIVIDUALSCOL=7
-META_WEIGHTCOL=8
+# META_SAMPLECOL=0
+# META_HOSTCOL=1
+# META_LOCATIONCOL=3
+# META_DATECOL=6
+# META_INDIVIDUALSCOL=7
+# META_WEIGHTCOL=8
 
-BLAST_QUERYIDCOl=0
-BLAST_SUBJECTIDCOl=1
-BLAST_IDENTITYCOl=2
-BLAST_LENGTHCOl=3
-BLAST_MISMATCHCOl=4
-BLAST_GAPOPENCOl=5
-BLAST_QUERYSTARTCOl=6
-BLAST_QUERYENDCOl=7
-BLAST_SUBJECTSTARTCOl=8
-BLAST_SUBJECTENDCOl=9
-BLAST_EVALUECOl=10
-BLAST_BITSCORECOl=11
+# BLAST_QUERYIDCOl=0
+# BLAST_SUBJECTIDCOl=1
+# BLAST_IDENTITYCOl=2
+# BLAST_LENGTHCOl=3
+# BLAST_MISMATCHCOl=4
+# BLAST_GAPOPENCOl=5
+# BLAST_QUERYSTARTCOl=6
+# BLAST_QUERYENDCOl=7
+# BLAST_SUBJECTSTARTCOl=8
+# BLAST_SUBJECTENDCOl=9
+# BLAST_EVALUECOl=10
+# BLAST_BITSCORECOl=11
 
-TAXO_ACCCOL=0
-TAXO_ORGANISMCOL=1
-TAXO_SUPKINGDOMCOL=2
-TAXO_LINEAGECOL=3
-TAXO_DEFCOL=4
+# TAXO_ACCCOL=0
+# TAXO_ORGANISMCOL=1
+# TAXO_SUPKINGDOMCOL=2
+# TAXO_LINEAGECOL=3
+# TAXO_DEFCOL=4
+
+# DEFAULT="."
 
 ########################################################################
 #Options
@@ -107,215 +113,215 @@ SHORTFLASH=sPID+"_All.FLASH.contigs2sample.tsv"
 
 ########################################################################
 #Function 	
-def LoadMetadata(sFile):
-	print("Loading file "+str(sFile))
-	dDict={}
-	for sNewLine in open(sFile):
-		sLine=sNewLine.replace("\n","")
-		if len(sLine)==0:
-			continue
-		tLine=sLine.split("\t")
+# def LoadMetadata(sFile):
+	# print("Loading file "+str(sFile))
+	# dDict={}
+	# for sNewLine in open(sFile):
+		# sLine=sNewLine.replace("\n","")
+		# if len(sLine)==0:
+			# continue
+		# tLine=sLine.split("\t")
 		
-		sHost=DEFAULT
-		if tLine[META_HOSTCOL]!="":
-			sHost=tLine[META_HOSTCOL]
-		sLocation=DEFAULT
-		if tLine[META_LOCATIONCOL]!="":
-			sLocation=tLine[META_LOCATIONCOL]
-		sDate=DEFAULT
-		if tLine[META_DATECOL]!="":
-			sDate=tLine[META_DATECOL]
-		sIndividuals=DEFAULT
-		if tLine[META_INDIVIDUALSCOL]!="":
-			sIndividuals=tLine[META_INDIVIDUALSCOL]
-		sWeight=DEFAULT
-		if tLine[META_WEIGHTCOL]!="":
-			sWeight=tLine[META_WEIGHTCOL]
+		# sHost=DEFAULT
+		# if tLine[META_HOSTCOL]!="":
+			# sHost=tLine[META_HOSTCOL]
+		# sLocation=DEFAULT
+		# if tLine[META_LOCATIONCOL]!="":
+			# sLocation=tLine[META_LOCATIONCOL]
+		# sDate=DEFAULT
+		# if tLine[META_DATECOL]!="":
+			# sDate=tLine[META_DATECOL]
+		# sIndividuals=DEFAULT
+		# if tLine[META_INDIVIDUALSCOL]!="":
+			# sIndividuals=tLine[META_INDIVIDUALSCOL]
+		# sWeight=DEFAULT
+		# if tLine[META_WEIGHTCOL]!="":
+			# sWeight=tLine[META_WEIGHTCOL]
 		
-		dDict[tLine[META_SAMPLECOL]]={
-			"Host":sHost,
-			"Location":sLocation,
-			"Date":sDate,
-			"Individuals":sIndividuals,
-			"Weight":sWeight
-			}
+		# dDict[tLine[META_SAMPLECOL]]={
+			# "Host":sHost,
+			# "Location":sLocation,
+			# "Date":sDate,
+			# "Individuals":sIndividuals,
+			# "Weight":sWeight
+			# }
 			
-	return dDict
+	# return dDict
 
-def LoadContigs(sFile,dRef,dDict={}):
-	print("Loading file "+str(sFile))
-	for sNewLine in open(sFile):
-		sLine=sNewLine.strip()
-		tLine=sLine.split()
-		sName=tLine[0]
-		try:
-			oCrash=dRef[sName]
-			dDict[sName]=tLine[1].split(SAMPLE_SEPARATOR)
-		except KeyError:
-			continue
-	return dDict
+# def LoadContigs(sFile,dRef,dDict={}):
+	# print("Loading file "+str(sFile))
+	# for sNewLine in open(sFile):
+		# sLine=sNewLine.strip()
+		# tLine=sLine.split()
+		# sName=tLine[0]
+		# try:
+			# oCrash=dRef[sName]
+			# dDict[sName]=tLine[1].split(SAMPLE_SEPARATOR)
+		# except KeyError:
+			# continue
+	# return dDict
 
-def LoadTaxo(sFile):
-	print("Loading file "+str(sFile))
-	dDict={}
-	for sNewLine in open(sFile):
-		sLine=sNewLine.strip()
-		tLine=sLine.split("\t")
+# def LoadTaxo(sFile):
+	# print("Loading file "+str(sFile))
+	# dDict={}
+	# for sNewLine in open(sFile):
+		# sLine=sNewLine.strip()
+		# tLine=sLine.split("\t")
 		
-		# print(tLine)
-		sOrganism=DEFAULT
-		if tLine[TAXO_ORGANISMCOL]!="":
-			sOrganism=tLine[TAXO_ORGANISMCOL]
-		sSuperkingdom=DEFAULT
-		if tLine[TAXO_SUPKINGDOMCOL]!="":
-			sSuperkingdom=tLine[TAXO_SUPKINGDOMCOL]
-		sLineage=DEFAULT
-		if tLine[TAXO_LINEAGECOL]!="":
-			sLineage=tLine[TAXO_LINEAGECOL]
-		sDefinition=DEFAULT
-		if tLine[TAXO_DEFCOL]!="":
-			sDefinition=tLine[TAXO_DEFCOL]
+		# # print(tLine)
+		# sOrganism=DEFAULT
+		# if tLine[TAXO_ORGANISMCOL]!="":
+			# sOrganism=tLine[TAXO_ORGANISMCOL]
+		# sSuperkingdom=DEFAULT
+		# if tLine[TAXO_SUPKINGDOMCOL]!="":
+			# sSuperkingdom=tLine[TAXO_SUPKINGDOMCOL]
+		# sLineage=DEFAULT
+		# if tLine[TAXO_LINEAGECOL]!="":
+			# sLineage=tLine[TAXO_LINEAGECOL]
+		# sDefinition=DEFAULT
+		# if tLine[TAXO_DEFCOL]!="":
+			# sDefinition=tLine[TAXO_DEFCOL]
 		
 		
-		dDict[tLine[TAXO_ACCCOL]]={
-			"Organism":sOrganism,
-			"Superkingdom":sSuperkingdom,
-			"Lineage":sLineage,
-			"Definition":sDefinition
-			}
+		# dDict[tLine[TAXO_ACCCOL]]={
+			# "Organism":sOrganism,
+			# "Superkingdom":sSuperkingdom,
+			# "Lineage":sLineage,
+			# "Definition":sDefinition
+			# }
 			
-	return dDict
+	# return dDict
 
-def LoadBlast(sFile):
-	print("Loading file "+str(sFile))
-	dDict={}
-	for sNewLine in open(sFile):
-		sLine=sNewLine.strip()
-		tLine=sLine.split("\t")
-		sQueryId=tLine[BLAST_QUERYIDCOl]
-		try:
-			oCrash=dDict[sQueryId]
-		except KeyError:
-			dDict[sQueryId]={}
+# def LoadBlast(sFile):
+	# print("Loading file "+str(sFile))
+	# dDict={}
+	# for sNewLine in open(sFile):
+		# sLine=sNewLine.strip()
+		# tLine=sLine.split("\t")
+		# sQueryId=tLine[BLAST_QUERYIDCOl]
+		# try:
+			# oCrash=dDict[sQueryId]
+		# except KeyError:
+			# dDict[sQueryId]={}
 		
-		sSubjectId=DEFAULT
-		if tLine[BLAST_SUBJECTIDCOl]!="":
-			sSubjectId=tLine[BLAST_SUBJECTIDCOl]
-			tSubjectId=sSubjectId.split("|")
-			sSubjectId=tSubjectId[3]
-		sIdentity=DEFAULT
-		if tLine[BLAST_IDENTITYCOl]!="":
-			sIdentity=tLine[BLAST_IDENTITYCOl]
-		sLength=DEFAULT
-		if tLine[BLAST_LENGTHCOl]!="":
-			sLength=tLine[BLAST_LENGTHCOl]
-		sMismatch=DEFAULT
-		if tLine[BLAST_MISMATCHCOl]!="":
-			sMismatch=tLine[BLAST_MISMATCHCOl]
-		sGapOpen=DEFAULT
-		if tLine[BLAST_GAPOPENCOl]!="":
-			sGapOpen=tLine[BLAST_GAPOPENCOl]
-		sQueryStart=DEFAULT
-		if tLine[BLAST_QUERYSTARTCOl]!="":
-			sQueryStart=tLine[BLAST_QUERYSTARTCOl]
-		sQueryEnd=DEFAULT
-		if tLine[BLAST_QUERYENDCOl]!="":
-			sQueryEnd=tLine[BLAST_QUERYENDCOl]
-		sSubjectStart=DEFAULT
-		if tLine[BLAST_SUBJECTSTARTCOl]!="":
-			sSubjectStart=tLine[BLAST_SUBJECTSTARTCOl]
-		sSubjectEnd=DEFAULT
-		if tLine[BLAST_SUBJECTENDCOl]!="":
-			sSubjectEnd=tLine[BLAST_SUBJECTENDCOl]
-		sEvalue=DEFAULT
-		if tLine[BLAST_EVALUECOl]!="":
-			sEvalue=tLine[BLAST_EVALUECOl]
-		sBitScore=DEFAULT
-		if tLine[BLAST_BITSCORECOl]!="":
-			sBitScore=tLine[BLAST_BITSCORECOl]
+		# sSubjectId=DEFAULT
+		# if tLine[BLAST_SUBJECTIDCOl]!="":
+			# sSubjectId=tLine[BLAST_SUBJECTIDCOl]
+			# tSubjectId=sSubjectId.split("|")
+			# sSubjectId=tSubjectId[3]
+		# sIdentity=DEFAULT
+		# if tLine[BLAST_IDENTITYCOl]!="":
+			# sIdentity=tLine[BLAST_IDENTITYCOl]
+		# sLength=DEFAULT
+		# if tLine[BLAST_LENGTHCOl]!="":
+			# sLength=tLine[BLAST_LENGTHCOl]
+		# sMismatch=DEFAULT
+		# if tLine[BLAST_MISMATCHCOl]!="":
+			# sMismatch=tLine[BLAST_MISMATCHCOl]
+		# sGapOpen=DEFAULT
+		# if tLine[BLAST_GAPOPENCOl]!="":
+			# sGapOpen=tLine[BLAST_GAPOPENCOl]
+		# sQueryStart=DEFAULT
+		# if tLine[BLAST_QUERYSTARTCOl]!="":
+			# sQueryStart=tLine[BLAST_QUERYSTARTCOl]
+		# sQueryEnd=DEFAULT
+		# if tLine[BLAST_QUERYENDCOl]!="":
+			# sQueryEnd=tLine[BLAST_QUERYENDCOl]
+		# sSubjectStart=DEFAULT
+		# if tLine[BLAST_SUBJECTSTARTCOl]!="":
+			# sSubjectStart=tLine[BLAST_SUBJECTSTARTCOl]
+		# sSubjectEnd=DEFAULT
+		# if tLine[BLAST_SUBJECTENDCOl]!="":
+			# sSubjectEnd=tLine[BLAST_SUBJECTENDCOl]
+		# sEvalue=DEFAULT
+		# if tLine[BLAST_EVALUECOl]!="":
+			# sEvalue=tLine[BLAST_EVALUECOl]
+		# sBitScore=DEFAULT
+		# if tLine[BLAST_BITSCORECOl]!="":
+			# sBitScore=tLine[BLAST_BITSCORECOl]
 					
-		dDict[sQueryId][len(dDict[sQueryId])+1]={
-			"SubjectId":sSubjectId,
-			"Identity":sIdentity,
-			"Length":sLength,
-			"Mismatch":sMismatch,
-			"GapOpen":sGapOpen,
-			"QueryStart":sQueryStart,
-			"QueryEnd":sQueryEnd,
-			"SubjectStart":sSubjectStart,
-			"SubjectEnd":sSubjectEnd,
-			"Evalue":sEvalue,
-			"BitScore":sBitScore
-			}
-	return dDict
+		# dDict[sQueryId][len(dDict[sQueryId])+1]={
+			# "SubjectId":sSubjectId,
+			# "Identity":sIdentity,
+			# "Length":sLength,
+			# "Mismatch":sMismatch,
+			# "GapOpen":sGapOpen,
+			# "QueryStart":sQueryStart,
+			# "QueryEnd":sQueryEnd,
+			# "SubjectStart":sSubjectStart,
+			# "SubjectEnd":sSubjectEnd,
+			# "Evalue":sEvalue,
+			# "BitScore":sBitScore
+			# }
+	# return dDict
 
-def LoadQuery(sFile):
-	print("Loading file "+str(sFile))
-	dDict={}
-	sSeqContent=""
-	sSeqName=""
-	for sNewLine in open(sFile):
-		if ">"==sNewLine[0]:
-			if sSeqName!="":
-				dDict[sSeqName]=sSeqContent
-			sSeqName=sNewLine[1:-1]
-			sSeqContent=""
-		else:
-			sSeqContent+=sNewLine[:-1] #:-1, for \n char
-	if ">"==sNewLine[0]:
-		if sSeqName!="":
-			dDict[sSeqName]=sSeqContent
-	return dDict
+# def LoadQuery(sFile):
+	# print("Loading file "+str(sFile))
+	# dDict={}
+	# sSeqContent=""
+	# sSeqName=""
+	# for sNewLine in open(sFile):
+		# if ">"==sNewLine[0]:
+			# if sSeqName!="":
+				# dDict[sSeqName]=sSeqContent
+			# sSeqName=sNewLine[1:-1]
+			# sSeqContent=""
+		# else:
+			# sSeqContent+=sNewLine[:-1] #:-1, for \n char
+	# if ">"==sNewLine[0]:
+		# if sSeqName!="":
+			# dDict[sSeqName]=sSeqContent
+	# return dDict
 
-def WriteData(FILE,dBlast,dTaxo,dContigs,dMetadata,dContent,dLength):
-	for sQuery in dBlast:
-		for iRank in dBlast[sQuery]:
+# def WriteData(FILE,dBlast,dTaxo,dContigs,dMetadata,dContent,dLength):
+	# for sQuery in dBlast:
+		# for iRank in dBlast[sQuery]:
 			
-			if iRank==1:
-				sRank=BEST_HIT
-			else:
-				sRank=HIT
+			# if iRank==1:
+				# sRank=BEST_HIT
+			# else:
+				# sRank=HIT
 			
-			iQuerySize=len(dContent[sQuery])
-			iCoverSize=int(dBlast[sQuery][iRank][QueryEnd])-int(dBlast[sQuery][iRank][QueryStart])
-			fCover=round(float(iCoverSize)/iQuerySize*100,2)
+			# iQuerySize=len(dContent[sQuery])
+			# iCoverSize=int(dBlast[sQuery][iRank][QueryEnd])-int(dBlast[sQuery][iRank][QueryStart])
+			# fCover=round(float(iCoverSize)/iQuerySize*100,2)
 			
-			try:
-				tSample=dContigs[sQuery]
-			except KeyError:
-				tSample=[sQuery.split("_")[-1]]
+			# try:
+				# tSample=dContigs[sQuery]
+			# except KeyError:
+				# tSample=[sQuery.split("_")[-1]]
 			
-			sReadQuantity=sQuery.split("(")[-1].split(")")[0]
-			sSubjectId=dBlast[sQuery][iRank]["SubjectId"]
+			# sReadQuantity=sQuery.split("(")[-1].split(")")[0]
+			# sSubjectId=dBlast[sQuery][iRank]["SubjectId"]
 			
-			sTaxo=dTaxo[sSubjectId]["Lineage"]
-			tTaxo=sTaxo.replace("; ",";").split(";")
-			iMinSize=0
-			for oTaxo in tTaxo:
-				try:
-					iMinSize=dLength[oTaxo]
-				except KeyError:
-					continue
-			if iMinSize==0:
-				fFragment="N/A"
-			else:
-				fFragment=round(float(iQuerySize)/iMinSize*100,2)
+			# sTaxo=dTaxo[sSubjectId]["Lineage"]
+			# tTaxo=sTaxo.replace("; ",";").split(";")
+			# iMinSize=0
+			# for oTaxo in tTaxo:
+				# try:
+					# iMinSize=dLength[oTaxo]
+				# except KeyError:
+					# continue
+			# if iMinSize==0:
+				# fFragment="N/A"
+			# else:
+				# fFragment=round(float(iQuerySize)/iMinSize*100,2)
 				
-			for sSample in tSample:
-				tLine=[sRank,sQuery,sSample,sReadQuantity,str(iQuerySize),
-				dMetadata[sSample]["Location"],dMetadata[sSample]["Date"],
-				dMetadata[sSample]["Host"],dMetadata[sSample]["Individuals"],
-				dMetadata[sSample]["Weight"],sSubjectId,
-				dTaxo[sSubjectId]["Organism"],dTaxo[sSubjectId]["Superkingdom"],
-				dTaxo[sSubjectId]["Lineage"],dTaxo[sSubjectId]["Definition"],dBlast[sQuery][iRank]["Confidence"],
-				str(fFragment),dBlast[sQuery][iRank]["Identity"],
-				str(fCover),dBlast[sQuery][iRank]["Length"],dBlast[sQuery][iRank]["Mismatch"],
-				dBlast[sQuery][iRank]["GapOpen"],dBlast[sQuery][iRank]["QueryStart"],
-				dBlast[sQuery][iRank]["QueryEnd"],dBlast[sQuery][iRank]["SubjectStart"],
-				dBlast[sQuery][iRank]["SubjectEnd"],dBlast[sQuery][iRank]["Evalue"],
-				dBlast[sQuery][iRank]["BitScore"],dContent[sQuery]
-				]
-				FILE.write("\t".join(tLine)+"\n")
+			# for sSample in tSample:
+				# tLine=[sRank,sQuery,sSample,sReadQuantity,str(iQuerySize),
+				# dMetadata[sSample]["Location"],dMetadata[sSample]["Date"],
+				# dMetadata[sSample]["Host"],dMetadata[sSample]["Individuals"],
+				# dMetadata[sSample]["Weight"],sSubjectId,
+				# dTaxo[sSubjectId]["Organism"],dTaxo[sSubjectId]["Superkingdom"],
+				# dTaxo[sSubjectId]["Lineage"],dTaxo[sSubjectId]["Definition"],dBlast[sQuery][iRank]["Confidence"],
+				# str(fFragment),dBlast[sQuery][iRank]["Identity"],
+				# str(fCover),dBlast[sQuery][iRank]["Length"],dBlast[sQuery][iRank]["Mismatch"],
+				# dBlast[sQuery][iRank]["GapOpen"],dBlast[sQuery][iRank]["QueryStart"],
+				# dBlast[sQuery][iRank]["QueryEnd"],dBlast[sQuery][iRank]["SubjectStart"],
+				# dBlast[sQuery][iRank]["SubjectEnd"],dBlast[sQuery][iRank]["Evalue"],
+				# dBlast[sQuery][iRank]["BitScore"],dContent[sQuery]
+				# ]
+				# FILE.write("\t".join(tLine)+"\n")
 						
 # HEADER_LIST=["Hit rank","Query Seq-Id","Sample","Read quantity","Sequence length","Location","Date","Host",
 # "Individual","Weight(mg)","Subject Seq-Id","Organism","SuperKingdom","Taxonomy","Hit definition","Identification","% Fragment","Identity",
@@ -402,16 +408,16 @@ def FusionBlastDict(dBlastN,dBlastX):
 					
 	return dDict
 
-def LoadLength(sFile):
-	print("Loading file "+str(sFile))
-	dDict={}
-	for sNewLine in open(sFile):
-		sLine=sNewLine.strip()
-		tLine=sLine.split()
-		sFamily=tLine[0]
-		iMinSize=int(tLine[1])
-		dDict[sFamily]=iMinSize
-	return dDict					
+# def LoadLength(sFile):
+	# print("Loading file "+str(sFile))
+	# dDict={}
+	# for sNewLine in open(sFile):
+		# sLine=sNewLine.strip()
+		# tLine=sLine.split()
+		# sFamily=tLine[0]
+		# iMinSize=int(tLine[1])
+		# dDict[sFamily]=iMinSize
+	# return dDict					
 
 ########################################################################
 #MAIN
