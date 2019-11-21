27a61ace8a0993e3287afd2dbee71417a488a268
ayantoine <aymeric.antoine@hotmail.fr>
2019-11-21 15:23:40 +0100

    Fix: Error during split of multiline seq fasta file. Add a script to fix it

--- Run_Blast.sh
+++ Run_Blast.sh
@@ -11,26 +11,19 @@ SEQMARKER="^>"
 echo ${SEQMARKER} > Target.txt
 nb_seq=$(grep -c -f Target.txt ${PID}_All.fa)
 nb_task=${SMAXSIMJOB}
-v_nb_seq_split=1000
-let $[ nb_seq_split=$nb_seq/$nb_task ]
-if [ $nb_seq_split == 0 ]
-	then
-	nb_seq_split=1
-elif [ $nb_seq_split -gt $v_nb_seq_split ]
-	then
-	nb_seq_split=$v_nb_seq_split
-fi
 
 mkdir ${PID}_ToBlast
-awk -v nb=$(($nb_seq_split*2)) 'NR%nb==1{file=FILENAME"."++i;}{print > file}' ${PID}_All.fa
-mv ${PID}_All.fa.* ${PID}_ToBlast
+
+echo "python ${SDIR}/SplitFasta.py -f ${PID}_ToBlast -i ${PID}_All.fa"
+python ${SDIR}/SplitFasta.py -f ${PID}_ToBlast -i ${PID}_All.fa
+
 nb_jobs=$(ls ${PID}_ToBlast | wc -l)
 
 echo "Number of initial sequences: "$nb_seq
 echo "Number of final sequences: "$(cat ${PID}"_ToBlast"/* | grep -c -f Target.txt)
-echo "Number of sequences per job: "$nb_seq_split
+echo "Number of sequences per job: "$(cat ${PID}"_ToBlast"/${PID}"_All.fa.1" | grep -c -f Target.txt)
 echo "Number of generated jobs: "$nb_jobs
-echo "Number of tasks: "$nb_task
+echo "Number of simultaneous tasks: "$nb_task
 echo "------ /Configure job array ------"
 rm Target.txt
 
@@ -38,7 +31,7 @@ rm Target.txt
 echo "------ Launch Blast by task ------"
 echo "$SCALL $SPARAM $SRENAME ${PID}_N_Blast -e Run_BlastN.e -o Run_BlastN.o ${SDIR}/Run_BlastTask.sh $ARG N"
 $SCALL $SPARAM $SRENAME ${PID}_N_Blast -e Run_BlastN.e -o Run_BlastN.o ${SDIR}/Run_BlastTask.sh $ARG N
-echo "$SCALL $SPARAM $SRENAME ${PID}_N_Blast -e Run_BlastN.e -o Run_BlastN.o ${SDIR}/Run_BlastTask.sh $ARG X"
+echo "$SCALL $SPARAM $SRENAME ${PID}_X_Blast -e Run_BlastX.e -o Run_BlastX.o ${SDIR}/Run_BlastTask.sh $ARG X"
 $SCALL $SPARAM $SRENAME ${PID}_X_Blast -e Run_BlastX.e -o Run_BlastX.o ${SDIR}/Run_BlastTask.sh $ARG X
 while [ ! -e ${PID}.BlastN.ok ]; do sleep 60 ; done
 while [ ! -e ${PID}.BlastX.ok ]; do sleep 60 ; done
---
+++ SplitFasta.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+"""Python3.6"""
+# compatibility: python2.7, python2.6
+
+import time
+from optparse import OptionParser
+
+sCurrentVersionScript="v1"
+iTime1=time.time()
+########################################################################
+'''
+V2-2019/11/21
+Split a fasta by chunck of 1000 into a specified folder
+
+python SplitFasta.py -f FOLDER -i INPUT
+INPUT: Fasta file
+FOLDER: Targeted folder
+'''
+########################################################################
+#CONSTANT
+CHUNCK=1000
+
+########################################################################
+#Options
+parser = OptionParser()
+parser.add_option("-f","--folder", dest="folder")
+parser.add_option("-i","--input", dest="input")
+
+(options, args) = parser.parse_args()
+
+sFolder=options.folder
+if not sFolder:
+	exit("Error : no folder -f defined, process broken")
+
+sInput=options.input
+if not sInput:
+	exit("Error : no input -i defined, process broken")
+
+########################################################################
+#Function 	
+def SplitFasta(sFolder,sInput):
+	iFileNumber=1
+	iSeqCount=0
+	FILE=open(sFolder+"/"+sInput+"."+str(iFileNumber))
+	sSeqName=""
+	sSeqContent=""
+	for sNewLine in open(sInput):
+		if ">"==sNewLine[0]:
+			if sSeqName!="":
+				FILE.write(sSeqName+sSeqContent)
+				iSeqCount+=1
+				if iSeqCount==CHUNCK:
+					FILE.close()
+					iFileNumber+=1
+					FILE=open(sFolder+"/"+sInput+"."+str(iFileNumber))
+				sSeqName=""
+				sSeqContent=""
+			sSeqName=sNewLine
+		else:
+			sSeqContent=sNewLine
+	if sSeqName!="":
+		FILE.write(sSeqName+sSeqContent)
+	FILE.close()
+
+########################################################################
+#MAIN
+if __name__ == "__main__":
+	SplitFasta(sFolder,sInput)
+	
+########################################################################    
+iTime2=time.time()
+iDeltaTime=iTime2-iTime1
+print("Script done: "+str(iDeltaTime))
+
