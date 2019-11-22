20afb1ad839396a9ebb4f5f21cfe311d380130c3
ayantoine <aymeric.antoine@hotmail.fr>
2019-11-22 11:03:01 +0100

    Add: Error recovery on blast array

--- BlastJob.sh
+++ BlastJob.sh
@@ -21,16 +21,19 @@ elif [ ${TASK} == X ]; then
     TAG="protein"
 fi
 
+if [ ! -f Blast${TASK}_Ok/${STASKID}_Blast${TASK}.ok ]; then
+    echo "------ Viral database blast ------"
+    echo "${BLAST} ${BLAST_OPT} -strand both -query ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -db ${VIRDB} -evalue 0.001 -max_target_seqs 1 -max_hsps 1 -outfmt 6 -out ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_1.tab"
+    ${BLAST} ${BLAST_OPT} -strand both -query ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -db ${VIRDB} -evalue 0.001 -max_target_seqs 1 -max_hsps 1 -outfmt 6 -out ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_1.tab
+    python ${SDIR}/SplitFastaOnBlastResults.py -f ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -b ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_1.tab -o ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}
+    echo "------ /Viral database blast ------"
 
-echo "------ Viral database blast ------"
-echo "${BLAST} ${BLAST_OPT} -strand both -query ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -db ${VIRDB} -evalue 0.001 -max_target_seqs 1 -max_hsps 1 -outfmt 6 -out ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_1.tab"
-${BLAST} ${BLAST_OPT} -strand both -query ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -db ${VIRDB} -evalue 0.001 -max_target_seqs 1 -max_hsps 1 -outfmt 6 -out ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_1.tab
-python ${SDIR}/SplitFastaOnBlastResults.py -f ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -b ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_1.tab -o ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}
-echo "------ /Viral database blast ------"
+    echo "------ Complete database blast ------"
+    echo "${BLAST} ${BLAST_OPT} -strand both -query ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.keeped -db ${ALLDB} -evalue 0.001 -max_target_seqs 5 -max_hsps 1 -outfmt 6 -out ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_2.tab"
+    ${BLAST} ${BLAST_OPT} -strand both -query ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.keeped -db ${ALLDB} -evalue 0.001 -max_target_seqs 5 -max_hsps 1 -outfmt 6 -out ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_2.tab
+    echo "------ /Complete database blast ------"
 
-echo "------ Complete database blast ------"
-echo "${BLAST} ${BLAST_OPT} -strand both -query ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.keeped -db ${ALLDB} -evalue 0.001 -max_target_seqs 5 -max_hsps 1 -outfmt 6 -out ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_2.tab"
-${BLAST} ${BLAST_OPT} -strand both -query ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.keeped -db ${ALLDB} -evalue 0.001 -max_target_seqs 5 -max_hsps 1 -outfmt 6 -out ${PID}"_Blast${TASK}"/${PID}_All.fa.${STASKID}.Blast${TASK}_2.tab
-echo "------ /Complete database blast ------"
-
-touch Blast${TASK}_Ok/${STASKID}_Blast${TASK}.ok
+    touch Blast${TASK}_Ok/${STASKID}_Blast${TASK}.ok
+else
+    echo "File Blast${TASK}_Ok/${STASKID}_Blast${TASK}.ok already present. Assume there is no need to launch Blast on this index-array."
+fi
