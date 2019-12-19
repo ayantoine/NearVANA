c52252bb787ccd241a3457277ba6070b436a01ee
ayantoine <aymeric.antoine@hotmail.fr>
2019-12-19 11:00:32 +0100

    Fix: Variable name

--- Run_Blast.sh
+++ Run_Blast.sh
@@ -30,9 +30,9 @@ rm Target.txt
 
 echo "------ Launch Blast by task ------"
 echo "$SCALL $SPARAM $SRENAME ${PID}_N_Blast -e Run_BlastN.e -o Run_BlastN.o ${SDIR}/Run_BlastTask.sh $ARG N"
-$SCALL $PARAM $SRENAME ${PID}_N_Blast -e Run_BlastN.e -o Run_BlastN.o ${SDIR}/Run_BlastTask.sh $ARG N
+$SCALL $SPARAM $SRENAME ${PID}_N_Blast -e Run_BlastN.e -o Run_BlastN.o ${SDIR}/Run_BlastTask.sh $ARG N
 echo "$SCALL $SPARAM $SRENAME ${PID}_X_Blast -e Run_BlastX.e -o Run_BlastX.o ${SDIR}/Run_BlastTask.sh $ARG X"
-$SCALL $PARAM $SRENAME ${PID}_X_Blast -e Run_BlastX.e -o Run_BlastX.o ${SDIR}/Run_BlastTask.sh $ARG X
+$SCALL $SPARAM $SRENAME ${PID}_X_Blast -e Run_BlastX.e -o Run_BlastX.o ${SDIR}/Run_BlastTask.sh $ARG X
 while [ ! -e ${PID}.BlastN.ok ]; do sleep 60 ; done
 while [ ! -e ${PID}.BlastX.ok ]; do sleep 60 ; done
 echo "------ /Launch Blast by task ------"
