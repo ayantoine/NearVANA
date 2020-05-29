b057231a1db95cee344ed1c182391829422071b4
ayantoine <aymeric.antoine@hotmail.fr>
2020-05-29 15:20:44 +0200

    Adjust option to contigs

--- Diamond.sh
+++ Diamond.sh
@@ -7,15 +7,15 @@ source $CONF
 nb_jobs=$(ls ${PID}_ToBlast | wc -l)
 
 if [ ! -f Diamond_Ok/${STASKID}_Diamond.ok ]; then
-    echo "------ Viral database blast ------"
-    echo "diamond blastx -d ${VIRPTDB_DIAMOND} -q ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -o ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.Diamond_1.tab -p ${MULTICPU} --max-target-seqs 1 --max-hsps 1 --evalue 0.001 --block-size $((${MULTIMEMORY}/6))"
-    diamond blastx -d ${VIRPTDB_DIAMOND} -q ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -o ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.Diamond_1.tab -p ${MULTICPU} --max-target-seqs 1 --max-hsps 1 --evalue 0.001 --block-size $((${MULTIMEMORY}/6))
+    echo "------ Viral database diamond ------"
+    echo "diamond blastx --frameshift 15 -d ${VIRPTDB_DIAMOND} -q ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -o ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.Diamond_1.tab -p ${MULTICPU} --max-target-seqs 1 --max-hsps 1 --evalue 0.001 --block-size $((${MULTIMEMORY}/6))"
+    diamond blastx --frameshift 15 -d ${VIRPTDB_DIAMOND} -q ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -o ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.Diamond_1.tab -p ${MULTICPU} --max-target-seqs 1 --max-hsps 1 --evalue 0.001 --block-size $((${MULTIMEMORY}/6))
     python ${SDIR}/SplitFastaOnBlastResults.py -f ${PID}"_ToBlast"/${PID}_All.fa.${STASKID} -b ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.Diamond_1.tab -o ${PID}"_BlastD"/${PID}_All.fa.${STASKID}
     echo "------ /Viral database blast ------"
 
-    echo "------ Complete database blast ------"
-    echo "diamond blastx -d ${ALLPTDB_DIAMOND} -q ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.keeped -o ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.Diamond_2.tab -p ${MULTICPU} --max-target-seqs 5 --max-hsps 1 --evalue 0.001 --block-size $((${MULTIMEMORY}/6))"
-    diamond blastx -d ${ALLPTDB_DIAMOND} -q ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.keeped -o ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.Diamond_2.tab -p ${MULTICPU} --max-target-seqs 5 --max-hsps 1 --evalue 0.001 --block-size $((${MULTIMEMORY}/6))
+    echo "------ Complete database diamond ------"
+    echo "diamond blastx --frameshift 15 -d ${ALLPTDB_DIAMOND} -q ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.keeped -o ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.Diamond_2.tab -p ${MULTICPU} --max-target-seqs 5 --max-hsps 1 --evalue 0.001 --block-size $((${MULTIMEMORY}/6))"
+    diamond blastx --frameshift 15 -d ${ALLPTDB_DIAMOND} -q ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.keeped -o ${PID}"_BlastD"/${PID}_All.fa.${STASKID}.Diamond_2.tab -p ${MULTICPU} --max-target-seqs 5 --max-hsps 1 --evalue 0.001 --block-size $((${MULTIMEMORY}/6))
     echo "------ /Complete database blast ------"
 
     touch Diamond_Ok/${STASKID}_Diamond.ok
