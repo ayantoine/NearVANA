511ab5a08a7fce0022acbec90d68181880fa7bf1
ayantoine <aymeric.antoine@hotmail.fr>
2020-06-26 10:20:56 +0200

    Fix: Add +1 job when KEEPUNASSIGNED

--- Run_SplitReads.sh
+++ Run_SplitReads.sh
@@ -23,36 +23,31 @@ USE_KEEPUNASSIGNED="$(boolean "${UNASSIGNED}")"
 
 echo "------ Launch SplitReads array ------"
 nb_jobs=$(cut -f1 ${!VAR_DODE} | wc -l)
-echo $nb_jobs
-echo $USE_KEEPUNASSIGNED
 if [ "$USE_KEEPUNASSIGNED" = true ] ; then
-	echo $nb_jobs
 	let nb_jobs++
 fi
 
-echo $nb_jobs
+if [ ! -d "SplitReads${VARNAME}-${PAIR}_Ok" ] ; then mkdir "SplitReads${VARNAME}-${PAIR}_Ok" ; fi
+if [ ! -d ${PID}"_log_SplitReads${VARNAME}-${PAIR}" ] ; then mkdir ${PID}"_log_SplitReads${VARNAME}-${PAIR}" ; fi
+echo "$SCALL $SPARAM $SRENAME ${PID}_${PAIR}-SplitReads ${STASKARRAY}1-${nb_jobs}${SMAXTASK}${SMAXSIMJOB} -e ${PID}"_log_SplitReads${VARNAME}-${PAIR}"/${PID}_SplitReads${VARNAME}-${PAIR}.e${SPSEUDOTASKID} -o ${PID}"_log_SplitReads${VARNAME}-${PAIR}"/${PID}_SplitReads${VARNAME}-${PAIR}.o${SPSEUDOTASKID} ${SDIR}/SplitReads.sh ${ARG} ${FASTQ} ${PAIR} ${VARNAME}"
+ $SCALL $SPARAM $SRENAME ${PID}_${PAIR}-SplitReads ${STASKARRAY}1-${nb_jobs}${SMAXTASK}${SMAXSIMJOB} -e ${PID}"_log_SplitReads${VARNAME}-${PAIR}"/${PID}_SplitReads${VARNAME}-${PAIR}.e${SPSEUDOTASKID} -o ${PID}"_log_SplitReads${VARNAME}-${PAIR}"/${PID}_SplitReads${VARNAME}-${PAIR}.o${SPSEUDOTASKID} ${SDIR}/SplitReads.sh ${ARG} ${FASTQ} ${PAIR} ${VARNAME}
+while true ; do
+	if [ $(ls SplitReads${VARNAME}-${PAIR}_Ok/ | wc -l) -eq 0 ]
+		then
+		nbr_ok=0
+	else
+		nbr_ok=$(ls SplitReads${VARNAME}-${PAIR}_Ok/*.SplitReads.${VARNAME}-${PAIR}.ok | wc -l)
+	fi
+	if [ "${nbr_ok}" -eq "${nb_jobs}" ]
+		then
+		rm -r SplitReads${VARNAME}-${PAIR}_Ok
+		break
+	fi
+	sleep 60
+done
+echo "------ /Launch SplitReads array ------"
 
-#if [ ! -d "SplitReads${VARNAME}-${PAIR}_Ok" ] ; then mkdir "SplitReads${VARNAME}-${PAIR}_Ok" ; fi
-#if [ ! -d ${PID}"_log_SplitReads${VARNAME}-${PAIR}" ] ; then mkdir ${PID}"_log_SplitReads${VARNAME}-${PAIR}" ; fi
-#echo "$SCALL $SPARAM $SRENAME ${PID}_${PAIR}-SplitReads ${STASKARRAY}1-${nb_jobs}${SMAXTASK}${SMAXSIMJOB} -e ${PID}"_log_SplitReads${VARNAME}-${PAIR}"/${PID}_SplitReads${VARNAME}-${PAIR}.e${SPSEUDOTASKID} -o ${PID}"_log_SplitReads${VARNAME}-${PAIR}"/${PID}_SplitReads${VARNAME}-${PAIR}.o${SPSEUDOTASKID} ${SDIR}/SplitReads.sh ${ARG} ${FASTQ} ${PAIR} ${VARNAME}"
-# $SCALL $SPARAM $SRENAME ${PID}_${PAIR}-SplitReads ${STASKARRAY}1-${nb_jobs}${SMAXTASK}${SMAXSIMJOB} -e ${PID}"_log_SplitReads${VARNAME}-${PAIR}"/${PID}_SplitReads${VARNAME}-${PAIR}.e${SPSEUDOTASKID} -o ${PID}"_log_SplitReads${VARNAME}-${PAIR}"/${PID}_SplitReads${VARNAME}-${PAIR}.o${SPSEUDOTASKID} ${SDIR}/SplitReads.sh ${ARG} ${FASTQ} ${PAIR} ${VARNAME}
-#while true ; do
-	#if [ $(ls SplitReads${VARNAME}-${PAIR}_Ok/ | wc -l) -eq 0 ]
-		#then
-		#nbr_ok=0
-	#else
-		#nbr_ok=$(ls SplitReads${VARNAME}-${PAIR}_Ok/*.SplitReads.${VARNAME}-${PAIR}.ok | wc -l)
-	#fi
-	#if [ "${nbr_ok}" -eq "${nb_jobs}" ]
-		#then
-		#rm -r SplitReads${VARNAME}-${PAIR}_Ok
-		#break
-	#fi
-	#sleep 60
-#done
-#echo "------ /Launch SplitReads array ------"
-
-#touch ${FASTQ}.split.ok
+touch ${FASTQ}.split.ok
 
 datetime2=$(date +%s)
 delta=$((datetime2 - datetime1))
