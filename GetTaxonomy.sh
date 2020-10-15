c843ff9a30194aedabd662c43824dd2e9b4cc513
ayantoine <aymeric.antoine@hotmail.fr>
2020-10-15 15:24:35 +0200

    /debug

--- GetTaxonomy.sh
+++ GetTaxonomy.sh
@@ -38,24 +38,24 @@ if [ ! -f Taxo${Task}_Ok/${STASKID}_Taxo.ok ] ; then
 	echo ^${ACC} > ${STASKID}.${ACC}.target.txt
 	while [ ! -e ${STASKID}.${ACC}.target.txt ]; do sleep 1 ; done
 	
-	TAXID=""
-	TEST=0
-	while true
-	    do
-	    let "TEST++"
-	    TAXID=$(grep -m 1 -f ${STASKID}.${ACC}.target.txt ${DBTARGET} | cut -f2)
-	    #echo ${TEST}":"${TAXID}
-	    if [ ${#TAXID} -ge 2 ]; then
-		break
-	    fi
-	    if [[ ${TEST} -eq 5 ]] ; then
-		break
-	    fi
-	done
-	echo "DEBUG=\$(grep -m 1 -f ${STASKID}.${ACC}.target.txt ${DBTARGET} | cut -f2)"
+	#TAXID=""
+	#TEST=0
+	#while true
+	    #do
+	    #let "TEST++"
+	    #TAXID=$(grep -m 1 -f ${STASKID}.${ACC}.target.txt ${DBTARGET} | cut -f2)
+	    ##echo ${TEST}":"${TAXID}
+	    #if [ ${#TAXID} -ge 2 ]; then
+		#break
+	    #fi
+	    #if [[ ${TEST} -eq 5 ]] ; then
+		#break
+	    #fi
+	#done
+	#echo "DEBUG=\$(grep -m 1 -f ${STASKID}.${ACC}.target.txt ${DBTARGET} | cut -f2)"
 	#echo "Test "${TEST}
 	
-	#TAXID=$(grep -m 1 -f ${STASKID}.${ACC}.target.txt ${DBTARGET} | cut -f2)
+	TAXID=$(grep -m 1 -f ${STASKID}.${ACC}.target.txt ${DBTARGET} | cut -f2)
 	echo ${ACC}"\t"${TAXID}
 	echo "^"${TAXID}"\t" > ${STASKID}.${ACC}.taxid.txt 
 	while [ ! -e ${STASKID}.${ACC}.taxid.txt ]; do sleep 1 ; done
