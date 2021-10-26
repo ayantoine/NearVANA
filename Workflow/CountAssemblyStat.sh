d93248df01af4c4ac91ff172b8e1394ea38dcb87
ayantoine <aymeric.antoine@hotmail.fr>
2021-10-26 14:45:40 +0200

    Fix: Wrong file parsed

--- Workflow/CountAssemblyStat.sh
+++ Workflow/CountAssemblyStat.sh
@@ -6,8 +6,8 @@ source $CONF
 SDIR=${GITDIR}/Workflow
 
 echo "------ Write stat ------"
-echo "python ${SDIR}/CountAssemblyStat.py -0 ${PID}_R0.Substracted.fastq -1 ${PID}_R1.Substracted.fastq -2 ${PID}_R2.Substracted.fastq -u ${PID}_All.Megahit_unmappedReads.tsv -o ${PID}_Stat_Assembly.tsv -d ${PID}_Demultiplexing_Hyper_Distribution.tsv"
-python ${SDIR}/CountAssemblyStat.py -0 ${PID}_R0.Substracted.fastq -1 ${PID}_R1.Substracted.fastq -2 ${PID}_R2.Substracted.fastq -u ${PID}_All.Megahit_unmappedReads.tsv -o ${PID}_Stat_Assembly.tsv -d ${PID}_Demultiplexing_Hyper_Distribution.tsv
+echo "python ${SDIR}/CountAssemblyStat.py -0 ${PID}_R0.Substracted.fastq -1 ${PID}_R1.Substracted.fastq -2 ${PID}_R2.Substracted.fastq -u ${PID}_All.Megahit_unmappedReads.tsv -o ${PID}_Stat_Assembly.tsv -d ${DATA}"
+python ${SDIR}/CountAssemblyStat.py -0 ${PID}_R0.Substracted.fastq -1 ${PID}_R1.Substracted.fastq -2 ${PID}_R2.Substracted.fastq -u ${PID}_All.Megahit_unmappedReads.tsv -o ${PID}_Stat_Assembly.tsv -d ${DATA}
 echo "------ /Write stat ------"
 
 echo "------ Create ok tagfile ------"
