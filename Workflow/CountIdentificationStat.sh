ba0eef91e45351b831f33f472c631fdc5f9893bd
ayantoine <aymeric.antoine@hotmail.fr>
2021-11-04 14:56:22 +0100

    Fix: Missing TASK variable

--- Workflow/CountIdentificationStat.sh
+++ Workflow/CountIdentificationStat.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 ARG=$1
+TASK=$2
 source $ARG
 source $CONF
 SDIR=${GITDIR}/Workflow
+
 
 echo "------ Write stat ------"
 echo "python ${SDIR}/CountIdentificationStat.py -t ${PID}_Blast${TASK}_results.tab -s ${PID}_Stat_Assembly.tsv -o ${PID}_Stat_Identificaion-${TASK}.tsv"
