1a6266a33793cb4ad3bb7dee49803723f6d3457a
ayantoine <aymeric.antoine@hotmail.fr>
2022-02-24 11:15:22 +0100

    Hack: Sample by Sample assembly

---
+++ Workflow/Hack_SplitSubstractedBySample.py
@@ -0,0 +1,71 @@
+# coding: utf-8
+"""Python3.6"""
+
+import time
+from optparse import OptionParser
+import os
+
+sCurrentVersionScript="v19"
+iTime1=time.time()
+########################################################################
+'''
+V1-2022/02/22
+
+python SplitSubstractedBySample.py -i INPUT
+
+INPUT: Fastq files
+'''
+########################################################################
+#CONSTANT
+UNDERSCORE="_"
+
+########################################################################
+#Options
+parser = OptionParser(conflict_handler="resolve")
+parser.add_option("-i","--input", dest="input")
+
+(options, args) = parser.parse_args()
+
+sInput=options.input
+if not sInput:
+	exit("Error : no input -i defined, process broken")
+
+########################################################################
+#Function
+def CreateFolder(sPath):
+    try:
+        os.mkdir(sPath)
+    except OSError or FileExistsError:
+        print("Warning: Can not create output folder {}".format(sFolder))
+
+def ParseInput(sFastq):
+    sPreviousSample=""
+    iIndex=0
+    for sNewLine in open(sFastq):
+        iIndex+=1
+        sContent+=sNewLine
+        if iIndex%4==1:
+            sLine=sNewLine.strip()
+            sSample=sLine.split(UNDERSCORE)[-1]
+        if iIndex%4==0:
+            if sPreviousSample!=sSample:
+                try:
+                    FILE.close()
+                except NameError:
+                    pass
+                CreateFolder(sSample)
+                FILE=open(sFolder+"/"+sInput)
+            FILE.write(sContent)    
+            sContent=""
+    FILE.close()
+
+########################################################################
+#MAIN
+if __name__ == "__main__":
+    ParseInput(sInput)
+
+########################################################################    
+iTime2=time.time()
+iDeltaTime=iTime2-iTime1
+print("Script done: "+str(iDeltaTime))
+
